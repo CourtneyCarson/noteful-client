1ef0da21c77af24328ca30de8ad321bd8f8e8c54
Courtney Carson <cmacarson@gmail.com>
2020-11-03 17:03:34 -0800

    working through folder router/service

--- seeds/seed.noteful.sql
+++ seeds/seed.noteful.sql
@@ -1,26 +1,13 @@
-INSERT INTO folders (name)
+INSERT INTO folders (folder_name)
 VALUES
 ('Made Up Words'), 
 ('Folder Names'), 
 ('Name of Folder'); 
 
-INSERT INTO notes (id, note_name, content, modified, )
+INSERT INTO notes (id, note_name, content, modified)
 VALUES 
-(
-  (SELECT id FROM folders WHERE name = 'Made Up Words'), 
-'dis is a note', 
-'lorem ipsum, yadda yadda',
- now()
-),
-(
-  (SELECT id FROM folders WHERE name = 'Folder Names'), 
-'dis is a note', 
-'lorem ipsum, words, words',
- now()
-),
-(
-  (SELECT id FROM folders WHERE name = 'Name Of Folder'), 
-'dis is a note', 
-'lorem ipsum, some more words here',
- now()
-);
+(1, 'dis is a note', 'lorem ipsum, yadda yadda', now() ),
+(2,'dis is a note', 'lorem ipsum, words, words', now() ),
+(3,'dis is a note', 'lorem ipsum, some more words here', now());
+
+
