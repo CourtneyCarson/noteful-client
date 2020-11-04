1257c101e31930fb021d4a0032f71f61ac1d43da
Courtney Carson <cmacarson@gmail.com>
2020-11-03 17:37:19 -0800

    .get, .post notes

---
+++ notes/notes-router.js
@@ -0,0 +1,100 @@
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const notesService = require('./notes-service')
+
+const notesRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeNotes = notes => ({
+  ... notes,
+  name: xss(notes.note_name),
+  content: xss(notes.content),
+})
+
+notesRouter
+  .route('/')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    notesService.getAllNotes(knexInstance)
+      .then(notes => {
+        res.json(notes.map(serializeNotes))
+      })
+      .catch(next)
+  })
+
+  .post(jsonParser, (req, res, next) => {
+    const { id, note_name, content, modified} = req.body
+    const newNote = { id, note_name, content, modified}
+
+    notesService.insertNote(
+      req.app.get('db'),
+      newNote
+    )
+      .then(note => {
+        res
+          .status(201)
+          .location(req.originalUrl + `/${note.id}`)
+          .json(serializeNotes(note))
+      })
+      .catch(next)
+  })
+
+  /// left off here 
+
+
+commentsRouter
+  .route('/:comment_id')
+  .all((req, res, next) => {
+    CommentsService.getById(
+      req.app.get('db'),
+      req.params.comment_id
+    )
+      .then(comment => {
+        if (!comment) {
+          return res.status(404).json({
+            error: { message: `Comment doesn't exist` }
+          })
+        }
+        res.comment = comment
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeComment(res.comment))
+  })
+  .delete((req, res, next) => {
+    CommentsService.deleteComment(
+      req.app.get('db'),
+      req.params.comment_id
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { text, date_commented } = req.body
+    const commentToUpdate = { text, date_commented }
+
+    const numberOfValues = Object.values(commentToUpdate).filter(Boolean).length
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain either 'text' or 'date_commented'`
+        }
+      })
+
+    CommentsService.updateComment(
+      req.app.get('db'),
+      req.params.comment_id,
+      commentToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+
+module.exports = commentsRouter
