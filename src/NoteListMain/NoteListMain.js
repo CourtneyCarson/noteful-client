1235a3602824a111b753e9c5d1081d3cb6871b2b
Courtney Carson <cmacarson@gmail.com>
2020-11-16 16:25:09 -0800

    work on context

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -4,37 +4,50 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Note from '../Note/Note'
 import CircleButton from '../CircleButton/CircleButton'
 import './NoteListMain.css'
+import { getNotesForFolder } from '../notes-helpers'
+import ApiContext from "../ApiContext";
 
-export default function NoteListMain(props) {
-  return (
-    <section className='NoteListMain'>
-      <ul>
-        {props.notes.map(note =>
-          <li key={note.id}>
-            <Note
-              id={note.id}
-              name={note.name}
-              modified={note.modified}
-            />
-          </li>
-        )}
-      </ul>
-      <div className='NoteListMain__button-container'>
-        <CircleButton
-          tag={Link}
-          to='/add-note'
-          type='button'
-          className='NoteListMain__add-note-button'
-        >
-          <FontAwesomeIcon icon='plus' />
-          <br />
+
+export default class NoteListMain extends React.Component {
+  static defaultProps = {
+    match: {
+      params: {},
+    },
+  }
+  static contextType = ApiContext
+
+  render() {
+    const { folder_id } = this.props.match.params
+    const { notes = [] } = this.context
+    const addNotesToFolder = getNotesForFolder(notes, folder_id)
+
+    return (
+      <section className='NoteListMain'>
+        <ul>
+          {addNotesToFolder.map((note) => (
+            <li key={note.id}>
+              <Note
+                id={note.id}
+                name={note.name}
+                modified={note.modified}
+              />
+            </li>
+          ))}
+        </ul>
+        <div className='NoteListMain__button-container'>
+          <CircleButton
+            tag={Link}
+            to='/add-note'
+            type='button'
+            className='NoteListMain__add-note-button'
+          >
+            <FontAwesomeIcon icon='plus' />
+            <br />
           Note
         </CircleButton>
-      </div>
-    </section>
-  )
-}
+        </div>
+      </section>
+    )
+  }
 
-NoteListMain.defaultProps = {
-  notes: [],
 }
